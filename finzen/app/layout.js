36f251ee75dee74291b41ccf9214fb052593781b
ParamPatel14 <parampraman59@gmail.com>
2025-04-02 07:33:42 +0530

    Intial Commit

---
+++ finzen/app/layout.js
@@ -0,0 +1,37 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+
+export const metadata = {
+  title: "Finzen",
+  description: "One Stop Finance Zening",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header></Header>
+
+
+
+
+
+
+          <main className="min-h-screen">{children}</main>
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-grey-600">
+              <p>Made With Intelligence</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
