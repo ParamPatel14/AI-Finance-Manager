a49f7da8c423b0167d5a960f209da01ed4dc42c3
ParamPatel14 <parampraman59@gmail.com>
2025-04-04 07:30:40 +0530

    Frontend2

--- finzen/app/page.js
+++ finzen/app/page.js
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
-
+import HeroSection from "@/components/hero";
 
 export default function Home() {
   return ( 
     <div className="mt-40">
-        <Button variant = "destructive">Hello Omoshiroi</Button>
+      <HeroSection />
+        
     </div>
   );
 }
---
+++ finzen/components/hero.jsx
@@ -0,0 +1,60 @@
+"use client";
+import { Button } from './ui/button';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useEffect, useRef } from 'react';
+
+const HeroSection = () => {
+    const imageref = useRef();
+
+    useEffect(() => {
+        const imageElement = imageref.current;
+
+        const handlescroll = () => {
+            const scrollPosition = window.scrollY;
+            const scrollThreshold = 100;
+            if(scrollPosition>scrollThreshold){
+                imageElement.classList.add('scrolled');
+            }else{
+                imageElement.classList.remove('scrolled');
+
+            }
+
+        window.addEventListener('scroll',handlescroll);
+    }
+}, []);
+
+
+  return ( 
+    <div className='pb-20 px-4'>
+        <div className='container mx-auto text-center'>
+            <h1 className='text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title'>
+                Manage Your Finances <br/> with Intelligence
+            </h1>
+            <p className = "text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
+                An AI-Powered finanacial managemnet platform that helps you track,analyse,and optimize your spending with real-time insights.
+            </p>
+            <div className='flex justify-center space-x-4'>
+                <Link href = "/dashboard">
+                    <Button>Get Started</Button>
+                </Link>
+            </div>
+            <div className='hero-image-wrapper'>
+                <div ref={imageref} className='hero-image'>
+                    <Image src= '/banner.jpeg'
+                    width = {1230}
+                    height = {720}
+                    alt = "Dasjboard Preview"
+                    className='rounded-lg shadow-2xl border mx-auto'
+                    priority>
+
+                    </Image>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+};
+
+export default HeroSection;
